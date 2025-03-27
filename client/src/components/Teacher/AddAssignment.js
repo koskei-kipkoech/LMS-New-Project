e332aeb5cf4ea00f3f32f02c917417208d458f6d
koskei-kipkoech <patrickwayy@gmail.com>
2025-03-27 11:07:27 +0300

    Fix Unused values

--- client/src/components/Teacher/AddAssignment.js
+++ client/src/components/Teacher/AddAssignment.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Container, Form, Button, Alert } from 'react-bootstrap';
+import {  Form, Button, Alert } from 'react-bootstrap';
 import TeacherSidebar from './sidebar';
 
 const AddAssignment = () => {
